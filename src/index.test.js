24128496fa0643f818b5b28cba8dccfaad197f39
David Konsumer <konsumer@jetboystudio.com>
2017-10-15 23:32:48 -0700

    formatting

--- src/index.test.js
+++ src/index.test.js
@@ -1,14 +1,14 @@
 /* global describe it expect */
 
 describe('basic tests', () => {
-	it('should multiply 2*2', () => {
-		expect(2*2).toEqual(4)
-		console.log('hello!')
-	})
-	it('should concat strings', () =>{
-		expect('hell'+'o!').toEqual('hello!')
-	})
-    it('should be able to add 1+1', () => {
+  it('should multiply 2*2', () => {
+    expect(2 * 2).toEqual(4)
+    console.log('hello!')
+  })
+  it('should concat strings', () => {
+    expect('hell' + 'o!').toEqual('hello!')
+  })
+  it('should be able to add 1+1', () => {
     expect(1 + 1).toEqual(2)
   })
 })
