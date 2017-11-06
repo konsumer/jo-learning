39aa9e0ec60a9adb6e803d90a043fc5a3fbc48cf
David Konsumer <konsumer@jetboystudio.com>
2017-11-06 15:01:25 -0800

    style

--- src/index.js
+++ src/index.js
@@ -1,6 +1,22 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-const App = () => (<div>APP</div>)
+import './index.scss'
+
+const App = () => (<div className='App'>
+  <header><h1>Johann Learning</h1></header>
+
+  <main>
+    <article>
+      <header>Article 1</header>
+      <main>
+        Uhm, at the start, she was a cook good, her vazhïn work well, and she strong on plow. But after three years, when she was 15, then she become weak. Her voice become a deep. "Borat, Borat!" She a receive hair on her chest and her vazhïn hang like sleeve of wizard.
+      </main>
+      <footer>Mon Nov  6 14:41:01 2017</footer>
+    </article>
+  </main>
+
+  <footer>Made with ❤ by konsumer & johann</footer>
+</div>)
 
 ReactDOM.render(<App />, document.getElementById('root'))
